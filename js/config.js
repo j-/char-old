206d9dd1d224ff5065690c930e4df3d825e2ffc0
j- <jh@live.com.au>
2015-01-15 00:30:42 +1100

    Added config and main files

---
+++ js/config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+	baseUrl: 'js',
+	paths: {
+		'bootstrap': '../lib/bootstrap/dist/js/bootstrap',
+		'jquery': '../lib/jquery/src/jquery',
+		'ok': '../lib/ok/ok',
+		'ok.views': '../lib/ok/ok.views',
+		'ok.dollarview': '../lib/ok/ok.dollarview',
+		'underscore': '../lib/underscore/underscore'
+	}
+});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+define(function (require) {
+	var App = require('controllers/App');
+	var app = new App();
+	window.app = app;
+});
