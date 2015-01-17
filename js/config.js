1c4e0b5bad16d83605c8063bbac226c4d6504681
j- <jh@live.com.au>
2015-01-17 16:41:59 +1100

    Lock Bootstrap, jQuery and OK down to latest releases

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ requirejs.config({
 	baseUrl: 'js',
 	paths: {
 		'bootstrap': '../lib/bootstrap/dist/js/bootstrap',
-		'jquery': '../lib/jquery/src/jquery',
+		'jquery': '../lib/jquery/dist/jquery',
 		'ok': '../lib/ok/ok',
 		'ok.views': '../lib/ok/ok.views',
 		'ok.dollarview': '../lib/ok/ok.dollarview',
