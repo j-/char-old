20b9641c2b6cbc94a6f37ff5df9a5dc23a9a8509
j- <jh@live.com.au>
2015-01-17 16:43:14 +1100

    Added encoding schemes data

---
+++ js/data/schemes.js
@@ -0,0 +1,81 @@
+define(function () {
+	return [
+		{
+			id: 'ch',
+			name: 'Character'
+		},
+		{
+			id: 'upper',
+			name: 'Upper-case'
+		},
+		{
+			id: 'lower',
+			name: 'Lower-case'
+		},
+		{
+			id: 'bin',
+			name: 'Binary'
+		},
+		{
+			id: 'oct',
+			name: 'Octal'
+		},
+		{
+			id: 'dec',
+			name: 'Decimal'
+		},
+		{
+			id: 'hex',
+			name: 'Hexadecimal'
+		},
+		{
+			id: 'css',
+			name: 'CSS'
+		},
+		{
+			id: 'html_named',
+			name: 'HTML',
+			variant: 'dec'
+		},
+		{
+			id: 'html_dec',
+			name: 'HTML',
+			variant: 'dec'
+		},
+		{
+			id: 'html_hex',
+			name: 'HTML',
+			variant: 'hex'
+		},
+		{
+			id: 'js_short',
+			name: 'JS',
+			variant: 'short'
+		},
+		{
+			id: 'js_long',
+			name: 'JS',
+			variant: 'long'
+		},
+		{
+			id: 'regex_oct',
+			name: 'RegEx',
+			variant: 'oct'
+		},
+		{
+			id: 'regex_hex',
+			name: 'RegEx',
+			variant: 'hex'
+		},
+		{
+			id: 'uri_escape',
+			name: 'URI',
+			variant: 'escape'
+		},
+		{
+			id: 'uri_forced',
+			name: 'URI',
+			variant: 'forced'
+		}
+	];
+});
