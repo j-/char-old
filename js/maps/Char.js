43430924309618268f8ab8365da03eb5c6dd8693
j- <jh@live.com.au>
2015-01-17 16:42:43 +1100

    Simplified CharMap schema definition

--- js/maps/Char.js
+++ js/maps/Char.js
@@ -1,44 +1,25 @@
 define(function (require) {
 	var ok = require('ok');
 
-	// properties
-	var Char = require('properties/Char');
-	var UpperCase = require('properties/UpperCase');
-	var LowerCase = require('properties/LowerCase');
-	var Binary = require('properties/Binary');
-	var Octal = require('properties/Octal');
-	var Decimal = require('properties/Decimal');
-	var Hexadecimal = require('properties/Hexadecimal');
-	var CSS = require('properties/CSS');
-	var HTMLEntityNamed = require('properties/HTMLEntityNamed');
-	var HTMLEntityDecimal = require('properties/HTMLEntityDecimal');
-	var HTMLEntityHexadecimal = require('properties/HTMLEntityHexadecimal');
-	var JSShort = require('properties/JSShort');
-	var JSLong = require('properties/JSLong');
-	var RegexOct = require('properties/RegexOct');
-	var RegexHex = require('properties/RegexHex');
-	var URIEscape = require('properties/URIEscape');
-	var URIForced = require('properties/URIForced');
-
 	var CharMap = ok.Map.extend({
 		schema: {
-			ch: Char,
-			upper: UpperCase,
-			lower: LowerCase,
-			bin: Binary,
-			oct: Octal,
-			dec: Decimal,
-			hex: Hexadecimal,
-			css: CSS,
-			html_named: HTMLEntityNamed,
-			html_dec: HTMLEntityDecimal,
-			html_hex: HTMLEntityHexadecimal,
-			js_short: JSShort,
-			js_long: JSLong,
-			regex_oct: RegexOct,
-			regex_hex: RegexHex,
-			uri_escape: URIEscape,
-			uri_forced: URIForced
+			ch:         require('properties/Char'),
+			upper:      require('properties/UpperCase'),
+			lower:      require('properties/LowerCase'),
+			bin:        require('properties/Binary'),
+			oct:        require('properties/Octal'),
+			dec:        require('properties/Decimal'),
+			hex:        require('properties/Hexadecimal'),
+			css:        require('properties/CSS'),
+			html_named: require('properties/HTMLEntityNamed'),
+			html_dec:   require('properties/HTMLEntityDecimal'),
+			html_hex:   require('properties/HTMLEntityHexadecimal'),
+			js_short:   require('properties/JSShort'),
+			js_long:    require('properties/JSLong'),
+			regex_oct:  require('properties/RegexOct'),
+			regex_hex:  require('properties/RegexHex'),
+			uri_escape: require('properties/URIEscape'),
+			uri_forced: require('properties/URIForced')
 		},
 		constructor: function Char (delegate) {
 			var schema = this.schema;
