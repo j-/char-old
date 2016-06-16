6fe425ff3b6e8466f804b1bff1b1c29c1d15b2ed
j- <j@skeoh.com>
2016-06-16 16:34:43 +1000

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const production = process.env.NODE_ENV === 'production';
+
+const plugins = [
+	new webpack.DefinePlugin({
+		'__DEV__': JSON.stringify(!production),
+	}),
+];
+
+if (production) {
+	plugins.push(
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production'),
+			},
+		})
+	);
+};
+
+module.exports = {
+	entry: {
+		'bundle': './src/index.jsx',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'build.js',
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel',
+				include: [
+					path.resolve(__dirname, 'src'),
+				],
+				test: /\.jsx?$/,
+				query: {
+					plugins: ['transform-runtime', 'transform-class-properties'],
+					presets: ['es2015', 'stage-2', 'react'],
+				},
+			},
+			{
+				loader: 'style!css!less',
+				include: [
+					path.resolve(__dirname, 'src/styles'),
+				],
+				test: /\.(css|less)$/,
+			},
+			{
+				loader: 'file',
+				test: /\.(eot|svg|ttf|woff2?)$/,
+			},
+		],
+	},
+	devtool: 'source-map',
+	plugins,
+};
