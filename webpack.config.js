f5218b2d563e0aae359d7a1cdc4e9f82d994d31c
j- <j@skeoh.com>
2016-06-16 16:38:15 +1000

    Fix bundle filename

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'build.js',
+		filename: 'bundle.js',
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx'],
