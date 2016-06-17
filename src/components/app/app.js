4a59bdd909a1e3adb69056f145f8b3c96cb6360f
j- <j@skeoh.com>
2016-06-17 11:11:11 +1000

    Add app component

---
+++ src/components/app/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+	<h1>Char</h1>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/app';
 import createStore from './create-store';
 
 const store = createStore();
+
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>,
+	document.getElementById('app')
+);
