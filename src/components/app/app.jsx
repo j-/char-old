ebf908f5b31f898ec06795fb294c05e3aeb89d33
j- <j@skeoh.com>
2016-06-18 01:24:02 +1000

    Add page header component to app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import Char from '../char';
 import ValueFields from '../value-fields';
 import { TYPE_CHAR } from '../../conversion';
+import { PageHeader } from 'react-bootstrap';
+
+const HEADER_TEXT = 'Unicode Characters';
+const HEADER_URL = 'https://en.wikipedia.org/wiki/Unicode';
+const HEADER_TITLE = '\'Unicode\' on Wikipedia';
 
 const App = ({ codePoint, isValid }) => (
 	<div className="container">
-		<h1>Char</h1>
+		<PageHeader>
+			<a href={ HEADER_URL } title={ HEADER_TITLE }>
+				{ HEADER_TEXT }
+			</a>
+		</PageHeader>
 		<div className="char-display">
 			<Char
 				codePoint={ codePoint }
