0f1e49ae5d400b99cabbe9534be5f09d8bdfc83a
j- <j@skeoh.com>
2016-06-18 00:49:38 +1000

    Add octal and binary conversion

--- src/conversion.js
+++ src/conversion.js
@@ -1,11 +1,15 @@
 export const TYPE_CHAR = 'char';
 export const TYPE_DECIMAL = 'decimal';
 export const TYPE_HEXADECIMAL = 'hexadecimal';
+export const TYPE_OCTAL = 'octal';
+export const TYPE_BINARY = 'binary';
 
 export const types = [
 	TYPE_CHAR,
 	TYPE_DECIMAL,
 	TYPE_HEXADECIMAL,
+	TYPE_OCTAL,
+	TYPE_BINARY,
 ];
 
 const throwInvalidType = (type) => {
@@ -21,6 +25,10 @@ export const convertFromCodePoint = (codePoint, type = TYPE_CHAR) => {
 			return String(codePoint);
 		case TYPE_HEXADECIMAL:
 			return Number(codePoint).toString(0x10);
+		case TYPE_OCTAL:
+			return Number(codePoint).toString(0o10);
+		case TYPE_BINARY:
+			return Number(codePoint).toString(0b10);
 		default:
 			throwInvalidType(type);
 	}
@@ -34,6 +42,10 @@ export const convertToCodePoint = (value, type = TYPE_CHAR) => {
 			return Number(value);
 		case TYPE_HEXADECIMAL:
 			return parseInt(value, 0x10);
+		case TYPE_OCTAL:
+			return parseInt(value, 0o10);
+		case TYPE_BINARY:
+			return parseInt(value, 0b10);
 		default:
 			throwInvalidType(type);
 	}
