f1a207d54d32a7f6da806af0c8ab82b69c6cdf56
j- <j@skeoh.com>
2016-06-17 14:31:58 +1000

    Add support for converting to code point

--- src/conversion.js
+++ src/conversion.js
@@ -6,14 +6,29 @@ export const types = [
 	TYPE_DECIMAL,
 ];
 
+const throwInvalidType = (type) => {
+	const names = types.map(type => `"${type}"`).join('/');
+	throw new TypeError(`Did not recognise conversion type "${type}". Expected ${names}.`);
+};
+
 export const convertFromCodePoint = (codePoint, type = TYPE_CHAR) => {
 	switch (type) {
 		case TYPE_CHAR:
 			return String.fromCodePoint(codePoint);
 		case TYPE_DECIMAL:
-			return Number(codePoint);
+			return String(codePoint);
 		default:
-			const names = types.map(type => `"${type}"`).join('/');
-			throw new TypeError(`Did not recognise conversion type "${type}". Expected ${names}.`);
+			throwInvalidType(type);
+	}
+};
+
+export const convertToCodePoint = (value, type = TYPE_CHAR) => {
+	switch (type) {
+		case TYPE_CHAR:
+			return String(value).charCodeAt(0);
+		case TYPE_DECIMAL:
+			return Number(value);
+		default:
+			throwInvalidType(type);
 	}
 };
