0ed4f4a01acd85de5099f7ecf770408b13efb5c8
j- <j@skeoh.com>
2016-06-18 00:54:17 +1000

    Add basic docblocks to conversion module

--- src/conversion.js
+++ src/conversion.js
@@ -17,6 +17,12 @@ const throwInvalidType = (type) => {
 	throw new TypeError(`Did not recognise conversion type "${type}". Expected ${names}.`);
 };
 
+/**
+ * @param {Number} codePoint Unicode value of character to convert
+ * @param {?String} type Type of value to convert into
+ * @returns {String} Converted value
+ * @throws {TypeError} If type is invalid
+ */
 export const convertFromCodePoint = (codePoint, type = TYPE_CHAR) => {
 	switch (type) {
 		case TYPE_CHAR:
@@ -34,6 +40,12 @@ export const convertFromCodePoint = (codePoint, type = TYPE_CHAR) => {
 	}
 };
 
+/**
+ * @param {String} value User input
+ * @param {?String} type Type of value to convert from
+ * @returns {String} Converted value
+ * @throws {TypeError} If type is invalid
+ */
 export const convertToCodePoint = (value, type = TYPE_CHAR) => {
 	switch (type) {
 		case TYPE_CHAR:
