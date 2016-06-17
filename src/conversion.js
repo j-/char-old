75691363445497eaa03e4528e794514f8b6df702
j- <j@skeoh.com>
2016-06-17 13:50:51 +1000

    Add decimal type to conversion module

--- src/conversion.js
+++ src/conversion.js
@@ -1,13 +1,17 @@
 export const TYPE_CHAR = 'char';
+export const TYPE_DECIMAL = 'decimal';
 
 export const types = [
 	TYPE_CHAR,
+	TYPE_DECIMAL,
 ];
 
 export const convertFromCodePoint = (codePoint, type = TYPE_CHAR) => {
 	switch (type) {
 		case TYPE_CHAR:
 			return String.fromCodePoint(codePoint);
+		case TYPE_DECIMAL:
+			return Number(codePoint);
 		default:
 			const names = types.map(type => `"${type}"`).join('/');
 			throw new TypeError(`Did not recognise conversion type "${type}". Expected ${names}.`);
