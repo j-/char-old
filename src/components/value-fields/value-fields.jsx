bb15a2a130e9c3d4b49b79e6dad589ea47b6bd63
j- <j@skeoh.com>
2016-06-18 01:12:59 +1000

    Add more conversion and value fields

--- src/components/value-fields/value-fields.jsx
+++ src/components/value-fields/value-fields.jsx
@@ -4,6 +4,11 @@ import {
 	TYPE_CHAR,
 	TYPE_DECIMAL,
 	TYPE_HEXADECIMAL,
+	TYPE_OCTAL,
+	TYPE_BINARY,
+	TYPE_CSS,
+	TYPE_JSLONG,
+	TYPE_JSSHORT,
 } from '../../conversion';
 
 const ValueFields = () => (
@@ -19,6 +24,26 @@ const ValueFields = () => (
 		<li>
 			<label>Hexadecimal</label>
 			<ValueField type={ TYPE_HEXADECIMAL } />
+		</li>
+		<li>
+			<label>Octal</label>
+			<ValueField type={ TYPE_OCTAL } />
+		</li>
+		<li>
+			<label>Binary</label>
+			<ValueField type={ TYPE_BINARY } />
+		</li>
+		<li>
+			<label>CSS</label>
+			<ValueField type={ TYPE_CSS } />
+		</li>
+		<li>
+			<label>JS (long)</label>
+			<ValueField type={ TYPE_JSLONG } />
+		</li>
+		<li>
+			<label>JS (short)</label>
+			<ValueField type={ TYPE_JSSHORT } />
 		</li>
 	</ul>
 );
--- src/conversion.js
+++ src/conversion.js
@@ -3,6 +3,9 @@ export const TYPE_DECIMAL = 'decimal';
 export const TYPE_HEXADECIMAL = 'hexadecimal';
 export const TYPE_OCTAL = 'octal';
 export const TYPE_BINARY = 'binary';
+export const TYPE_CSS = 'css';
+export const TYPE_JSLONG = 'jslong';
+export const TYPE_JSSHORT = 'jsshort';
 
 export const types = [
 	TYPE_CHAR,
@@ -10,12 +13,19 @@ export const types = [
 	TYPE_HEXADECIMAL,
 	TYPE_OCTAL,
 	TYPE_BINARY,
+	TYPE_CSS,
+	TYPE_JSLONG,
+	TYPE_JSSHORT,
 ];
 
 const throwInvalidType = (type) => {
 	const names = types.map(type => `"${type}"`).join('/');
 	throw new TypeError(`Did not recognise conversion type "${type}". Expected ${names}.`);
 };
+
+const lpadZeroes = (value, length) => (
+	'0'.repeat(Math.max(length - value.length, 0)) + value
+);
 
 /**
  * @param {Number} codePoint Unicode value of character to convert
@@ -35,6 +45,16 @@ export const convertFromCodePoint = (codePoint, type = TYPE_CHAR) => {
 			return Number(codePoint).toString(0o10);
 		case TYPE_BINARY:
 			return Number(codePoint).toString(0b10);
+		case TYPE_CSS:
+			return '\\' + codePoint.toString(0x10);
+		case TYPE_JSLONG:
+			return codePoint > 0xffff ?
+				null :
+				'\\u' + lpadZeroes(codePoint.toString(0x10), 4);
+		case TYPE_JSSHORT:
+			return codePoint > 0xff ?
+				null :
+				'\\x' + lpadZeroes(codePoint.toString(0x10), 2);
 		default:
 			throwInvalidType(type);
 	}
@@ -58,6 +78,12 @@ export const convertToCodePoint = (value, type = TYPE_CHAR) => {
 			return parseInt(value, 0o10);
 		case TYPE_BINARY:
 			return parseInt(value, 0b10);
+		case TYPE_CSS:
+			return parseInt(value.substring(1), 0x10);
+		case TYPE_JSLONG:
+			return parseInt(value.substring(2), 0x10);
+		case TYPE_JSSHORT:
+			return parseInt(value.substring(2, 4), 0x10);
 		default:
 			throwInvalidType(type);
 	}
