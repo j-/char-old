ede8d14efb70417656a4bf740a6544881af720a9
j- <j@skeoh.com>
2016-06-19 23:55:01 +1000

    Output actual characters from charmap

--- src/components/charmap/charmap.jsx
+++ src/components/charmap/charmap.jsx
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-export const CharmapCell = () => (
-	<td className="charmap-cell">&times;</td>
+export const CharmapCell = ({ offset }) => (
+	<td className="charmap-cell">
+		{ String.fromCodePoint(offset) }
+	</td>
 );
 
-export const CharmapRow = () => (
+export const CharmapRow = ({ offset, cols }) => (
 	<tr className="charmap-row">
 	{
-		Array(16).fill(null).map((_, i) => (
-			<CharmapCell key={ i } />
+		Array(cols).fill(null).map((_, i) => (
+			<CharmapCell
+				key={ i }
+				offset={ offset + i }
+			/>
 		))
 	}
 	</tr>
 );
 
-export const CharmapRows = () => (
+export const CharmapRows = ({ offset, rows, cols }) => (
 	<tbody className="charmap-body">
 	{
-		Array(16).fill(null).map((_, i) => (
-			<CharmapRow key={ i } />
+		Array(rows).fill(null).map((_, i) => (
+			<CharmapRow
+				key={ i }
+				offset={ offset + i * cols }
+				cols={ cols }
+			/>
 		))
 	}
 	</tbody>
 );
 
-const Charmap = () => {
+const Charmap = ({ page, rows, cols }) => {
 	return (
 		<table className="charmap">
-			<CharmapRows />
+			<CharmapRows
+				offset={ page * rows * cols }
+				rows={ rows }
+				cols={ cols }
+			/>
 		</table>
 	);
+};
+
+Charmap.propTypes = {
+	page: PropTypes.number,
+	rows: PropTypes.number,
+	cols: PropTypes.number,
+};
+
+Charmap.defaultProps = {
+	page: 0,
+	rows: 0x10,
+	cols: 0x10,
 };
 
 export default Charmap;
