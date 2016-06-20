5d649e08c28d343171f4b8b26ea7be01dc5cf3a7
j- <j@skeoh.com>
2016-06-20 23:16:00 +1000

    Reset last modified when updating code point

--- src/reducers/last-modified/index.js
+++ src/reducers/last-modified/index.js
@@ -1,11 +1,14 @@
 import {
 	SET_INPUT_VALUE,
+	SET_CODE_POINT,
 } from '../types';
 
 export default (state = null, action) => {
 	switch (action.type) {
 		case SET_INPUT_VALUE:
 			return action.inputType;
+		case SET_CODE_POINT:
+			return null;
 		default:
 			return state;
 	}
