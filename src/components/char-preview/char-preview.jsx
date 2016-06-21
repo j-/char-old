a33490e9ef514f4c21d8c930f952196dd1e686b8
j- <j@skeoh.com>
2016-06-21 11:03:09 +1000

    Wrap the character display in a Bootstrap panel

--- src/components/char-preview/char-preview.jsx
+++ src/components/char-preview/char-preview.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Char from '../char';
 import { TYPE_CHAR } from '../../conversion';
+import { Panel } from 'react-bootstrap';
 
 const CharPreview = ({ codePoint, isValid }) => (
-	<div className="char-display">
-		<Char
-			codePoint={ codePoint }
-			isValid={ isValid }
-			type={ TYPE_CHAR }
-		/>
-	</div>
+	<Panel>
+		<div className="char-display">
+			<Char
+				codePoint={ codePoint }
+				isValid={ isValid }
+				type={ TYPE_CHAR }
+			/>
+		</div>
+	</Panel>
 );
 
 export default CharPreview;
