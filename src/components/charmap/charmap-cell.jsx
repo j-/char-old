77f07c9e9797d1b7e59a06744806c1b376fc0f8e
j- <j@skeoh.com>
2016-06-22 22:31:45 +1000

    Clean up charmap cell component

--- src/components/charmap/charmap-cell.jsx
+++ src/components/charmap/charmap-cell.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
-const handler = (callback, ...args) => (e) => {
-	e.preventDefault();
-	callback(...args);
+const CharmapCell = ({ offset, setCodePoint, activeCodePoint }) => {
+	const onClick = (e) => {
+		e.preventDefault();
+		setCodePoint(offset);
+	};
+	const classNames = ['charmap-char'];
+	if (offset === activeCodePoint) {
+		classNames.push('active');
+	}
+	const className = classNames.join(' ');
+	const textContent = String.fromCodePoint(offset);
+	return (
+		<td className="charmap-cell">
+			<a className={ className } href="#" onClick={ onClick }>
+				<span className="charmap-char-wrapper">{ textContent }</span>
+			</a>
+		</td>
+	);
 };
-
-const CharmapCell = ({ offset, setCodePoint, activeCodePoint }) => (
-	<td className="charmap-cell">
-		<a
-			className={ 'charmap-char ' + (offset === activeCodePoint ? 'active' : '') }
-			href="#"
-			onClick={ handler(setCodePoint, offset) }
-		>
-			<span className="charmap-char-wrapper">
-				{ String.fromCodePoint(offset) }
-			</span>
-		</a>
-	</td>
-);
 
 export default CharmapCell;
