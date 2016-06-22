52097095825214c9c4fc268c5a03d96521ffe0f3
j- <j@skeoh.com>
2016-06-22 22:57:10 +1000

    Replace control characters with their acronyms

--- src/components/charmap/charmap-cell.jsx
+++ src/components/charmap/charmap-cell.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import acronyms from '../../data/acronyms';
+import classNames from 'classnames';
 
 const CharmapCell = ({ offset, setCodePoint, activeCodePoint }) => {
 	const onClick = (e) => {
 		e.preventDefault();
 		setCodePoint(offset);
 	};
-	const classNames = ['charmap-char'];
-	if (offset === activeCodePoint) {
-		classNames.push('active');
-	}
-	const className = classNames.join(' ');
-	const textContent = String.fromCodePoint(offset);
+	const isActive = offset === activeCodePoint;
+	const showAcronym = offset in acronyms;
+	const textContent = showAcronym ?
+		acronyms[offset] :
+		String.fromCodePoint(offset);
 	return (
-		<td className="charmap-cell">
-			<a className={ className } href="#" onClick={ onClick }>
+		<td className={
+			classNames('charmap-cell', isActive && 'active', showAcronym && 'acronym')
+		}>
+			<a className="charmap-char" href="#" onClick={ onClick }>
 				<span className="charmap-char-wrapper">{ textContent }</span>
 			</a>
 		</td>
